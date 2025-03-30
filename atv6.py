8ccfc8d2b5cfd708ce29f91fbff421306980b7f0
Pedro <81062412+joaoslk@users.noreply.github.com>
2025-03-29 21:50:58 -0300

    Update atv6.py

--- atv6.py
+++ atv6.py
@@ -1,5 +1,3 @@
-#Crie um programa que gere e exiba os 100 primeiros números primos.
-
 def analise_primo(n):
     if n < 2:
         return False
@@ -7,13 +5,10 @@ def analise_primo(n):
         if n % i == 0:
             return False
     return True
-
 primos = []
 num = 2
-
 while len(primos) < 100:
     if analise_primo(num):
         primos.append(num)
     num += 1
-
 print(" ".join(map(str, primos)))
