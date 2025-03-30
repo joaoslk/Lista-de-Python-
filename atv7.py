324591d43ce88ce9092322fc1b80f14405c5441c
Pedro <81062412+joaoslk@users.noreply.github.com>
2025-03-29 21:53:16 -0300

    Update atv7.py

--- atv7.py
+++ atv7.py
@@ -1,16 +1,9 @@
-#Solicite um número ímpar ao usuário e calcule a diferença entre o quadrado do
-#número anterior e do próximo número ímpar.
-
-numero = int(input("Diga um numero impar:"))
-
-anterior = numero - 2
+number = int(input("Diga um numero impar:"))
+anterior = number - 2
 num_anterior = anterior * anterior
-
-posterior = numero + 2
+posterior = number + 2
 num_posterior = posterior * posterior
-
-dif_anterior =  - numero 
-dif_posterior = num_posterior - numero 
-
+dif_anterior = num_anterior - number
+dif_posterior = num_posterior - number 
 print(f"Diferenca do quadrado do anterior: {dif_anterior}.")
 print(f"Diferenca do quadrado do posterior: {dif_posterior}.")
