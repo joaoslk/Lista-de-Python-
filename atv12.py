251b1f3c179ad846f00e811922fc155d3ab3bb46
Pedro <81062412+joaoslk@users.noreply.github.com>
2025-03-29 21:56:48 -0300

    Update atv12.py

--- atv12.py
+++ atv12.py
@@ -1,13 +1,7 @@
-#Peça ao usuário a quantidade de turmas e a quantidade total de alunos. Informe a
-#média de alunos por turma, mas avise se alguma turma tiver mais de 40 alunos.
-
 turmas = int(input("Informe a quantidade de turmas: "))
 alunos = int(input("Informe a quantidade total de alunos: "))
-
-media = alunos // turmas
-
-if media > 40:
-    print(f"Atencao, a media é superior a 40 alunos, sendo {media} alunos.")
-
+med = alunos // turmas
+if med > 40:
+    print(f"Atencao, a media é superior a 40 alunos, com um total de {media}.")
 else:
  print(f"A media de alunos por turma é {media}.")
