d6f56212cb4b233ff3b17cc431897d7f1eb92697
Pedro <81062412+joaoslk@users.noreply.github.com>
2025-03-29 21:43:42 -0300

    Update atv2.py

--- atv2.py
+++ atv2.py
@@ -1,9 +1,5 @@
-#Solicite um valor em segundos e converta para dias, horas e minutos.
-
-tempo = int(input("Diga o valor dos segundos:"))
-
-minutos = tempo // 60
-horas = minutos // 60
+seg = int(input("Diga o valor dos segundos:"))
+min = seg // 60
+horas = min // 60
 dias = horas // 24
-
-print(f"Dias: {dias}, Horas: {horas}, Minutos: {minutos}")
+print(f"Dias: {dias}, Horas: {horas}, Minutos: {min}")
